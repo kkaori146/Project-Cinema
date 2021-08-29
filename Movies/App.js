a720dc23ff5a3853467cd4e10e71abccfa515b65
Kaminishikawahara KK <kkaori146@gmail.com>
2021-08-29 20:35:53 +0900

    Project Completed

--- Movies/App.js
+++ Movies/App.js
@@ -1,58 +1,63 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SafeAreaView, Text, Button, FlatList, Image, View, StyleSheet, ActivityIndicator } from 'react-native';
 
 const App = () => {
 
   const [loading, setLoading] = useState(false);
-
   const [movies, setMovies] = useState([]);
 
-  const handleLoadButton = async () => {
-    const req = await fetch ("https://api.b7web.com.br/cinema/");
-    const json = await req.json();
+  useEffect(()=>{
+    const requestMovies = async () => {
+      setLoading(true);
+      
+      const req = await fetch("https://api.b7web.com.br/cinema/");
+      const json = await req.json();
 
-    if(json) {
-      setMovies(json);
+      if(json) {
+        setMovies(json);
+      }
+
+      setLoading(false);
     }
-  }
+
+    requestMovies();
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-     <Button title="Loading the movies" onPress={handleLoadButton}/>
       {loading &&
         <View style={styles.loadingArea}>
-          <ActivityIndicator size="large" color="#FFF" />
+          <ActivityIndicator size="large" color="#FFFFFF" />
           <Text style={styles.loadingText}>Loading...</Text>
         </View>
       }
-     {!loading && 
-     <>
-      <Text style={styles.totalMoviesText}>Total of Movies: {movies.length}</Text>
-     <FlatList
-        style={styles.list}
-          data={movies}
-          renderItem={({item})=> (
-            <View style={styles.movieItem}>
-              <Image 
-              source={{uri: item.avatar}} 
-              style={styles.movieImage}
-              resizeMode="contain"
-              />
-              <Text style={styles.movieTitle}>{item.titulo}</Text>
-            </View>
-          )}
-          keyExtractor={item => item.titulo}
-        />
-     </>
-     }
-     
+      {!loading &&
+        <>
+          <Text style={styles.totalMoviesText}>Movies: {movies.length}</Text>
+          <FlatList
+            style={styles.list}
+            data={movies}
+            renderItem={({item})=>(
+              <View style={styles.movieItem}>
+                <Image
+                  source={{uri: item.avatar}}
+                  style={styles.movieImage}
+                  resizeMode="contain"
+                />
+                <Text style={styles.movieTitle}>{item.titulo}</Text>
+              </View>
+            )}
+            keyExtractor={item => item.titulo}
+          />
+        </>
+      }      
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container:{
-    flex:1,
+    flex: 1,
     backgroundColor: '#333'
   },
   loadingArea:{
@@ -74,16 +79,16 @@ const styles = StyleSheet.create({
     flex: 1
   },
   movieItem:{
-    marginBottom:30
+    marginBottom: 30
   },
   movieImage:{
     height: 400
   },
   movieTitle:{
-    color:'#FFF',
+    color: '#FFF',
     fontSize: 24,
     textAlign: 'center',
-    marginTop:5
+    marginTop: 5
   }
 });
 
